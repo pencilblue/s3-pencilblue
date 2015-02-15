7e89cdafd7596263a75443d4251cd500df0d4a95
Brian Hyder <brianhyder@gmail.com>
2015-02-15 18:19:14 -0500

    Updates for 0.4.0

--- s3.js
+++ s3.js
@@ -1,56 +1,73 @@
-/**
- * S3 - A sample for exemplifying what the main module file should
- * look like.
- *
- * @author Brian Hyder <brian@pencilblue.org>
- * @copyright 2014 PencilBlue, LLC
- */
-function S3(){}
+/*
+    Copyright (C) 2015  PencilBlue, LLC
 
-/**
- * Called when the application is being installed for the first time.
- *
- * @param cb A callback that must be called upon completion.  cb(Error, Boolean).
- * The result should be TRUE on success and FALSE on failure
- */
-S3.onInstall = function(cb) {
-	cb(null, true);
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+module.exports = function S3Module(/*pb*/) {
+
+    /**
+     * Main Module file for the Amazon S3 Media Provider Plugin
+     * @class S3
+     */
+    function S3(){}
+
+    /**
+     * Called when the application is being installed for the first time.
+     *
+     * @param cb A callback that must be called upon completion.  cb(Error, Boolean).
+     * The result should be TRUE on success and FALSE on failure
+     */
+    S3.onInstall = function(cb) {
+        cb(null, true);
+    };
+
+    /**
+     * Called when the application is uninstalling this plugin.  The plugin should
+     * make every effort to clean up any plugin-specific DB items or any in function
+     * overrides it makes.
+     *
+     * @param cb A callback that must be called upon completion.  cb(Error, Boolean).
+     * The result should be TRUE on success and FALSE on failure
+     */
+    S3.onUninstall = function(cb) {
+        cb(null, true);
+    };
+
+    /**
+     * Called when the application is starting up. The function is also called at
+     * the end of a successful install. It is guaranteed that all core PB services
+     * will be available including access to the core DB.
+     *
+     * @param cb A callback that must be called upon completion.  cb(Error, Boolean).
+     * The result should be TRUE on success and FALSE on failure
+     */
+    S3.onStartup = function(cb) {
+        cb(null, true);
+    };
+
+    /**
+     * Called when the application is gracefully shutting down.  No guarantees are
+     * provided for how much time will be provided the plugin to shut down.
+     *
+     * @param cb A callback that must be called upon completion.  cb(Error, Boolean).
+     * The result should be TRUE on success and FALSE on failure
+     */
+    S3.onShutdown = function(cb) {
+        cb(null, true);
+    };
+
+    //exports
+    return S3;
 };
-
-/**
- * Called when the application is uninstalling this plugin.  The plugin should
- * make every effort to clean up any plugin-specific DB items or any in function
- * overrides it makes.
- *
- * @param cb A callback that must be called upon completion.  cb(Error, Boolean).
- * The result should be TRUE on success and FALSE on failure
- */
-S3.onUninstall = function(cb) {
-	cb(null, true);
-};
-
-/**
- * Called when the application is starting up. The function is also called at
- * the end of a successful install. It is guaranteed that all core PB services
- * will be available including access to the core DB.
- *
- * @param cb A callback that must be called upon completion.  cb(Error, Boolean).
- * The result should be TRUE on success and FALSE on failure
- */
-S3.onStartup = function(cb) {
-	cb(null, true);
-};
-
-/**
- * Called when the application is gracefully shutting down.  No guarantees are
- * provided for how much time will be provided the plugin to shut down.
- *
- * @param cb A callback that must be called upon completion.  cb(Error, Boolean).
- * The result should be TRUE on success and FALSE on failure
- */
-S3.onShutdown = function(cb) {
-	cb(null, true);
-};
-
-//exports
-module.exports = S3;
